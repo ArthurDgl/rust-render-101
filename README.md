b4265ead253dc56ab6e304f1099f0dbab5bbb5e5
ArthurDgl <arthur.dugnolle@gmail.com>
2024-11-03 17:27:51 +0100

    framerate, colors, fill, background, rect & example

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # rust-render-101
 An expansion library for minifb in rust. Includes primitive shapes and other features in a p5js style.
+
+Example Code :
+```rust
+// IMPORTS
+
+impl Runnable for Sketch {
+    fn setup(&mut self) {
+        println!("SETUP WAS CALLED");
+        self.framerate(60);
+    }
+
+    fn draw(&mut self) {
+        if self.frame_count == 0 {
+            println!("FIRST DRAW CALL");
+        }
+
+        let blue: u32 = Self::rbg_color(50, 50, 255);
+        let green: u32 = Self::rbg_color(50, 255, 50);
+
+        self.background(blue);
+
+        self.fill(green);
+        self.rect(50, 100, 200, 100);
+    }
+}
+
+fn main() {
+    Sketch::from_size(640, 480).run();
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -21,18 +21,20 @@ pub struct Sketch {
 
     mouse_x: f32,
     mouse_y: f32,
+
+    fill_color: u32,
 }
 
 impl Sketch {
     fn from_size(width: usize, height: usize) -> Sketch {
-        let mut window = minifb::Window::new(DEFAULT_NAME, width, height, minifb::WindowOptions::default())
+        let window = minifb::Window::new(DEFAULT_NAME, width, height, minifb::WindowOptions::default())
             .unwrap_or_else(|e| {
                 panic!("Unable to open window: {}", e);
             });
 
-        let mut pixels: Vec<u32> = vec![0; width*height];
+        let pixels: Vec<u32> = vec![0; width*height];
 
-        Sketch {window, pixels, width, height, is_looping: true, frame_count: 0, delta_time: 0.0, mouse_x: 0.0, mouse_y: 0.0}
+        Sketch {window, pixels, width, height, is_looping: true, frame_count: 0, delta_time: 0.0, mouse_x: 0.0, mouse_y: 0.0, fill_color: 0}
     }
 
     fn run(&mut self) {
@@ -61,6 +63,46 @@ impl Sketch {
     fn no_loop(&mut self) {
         self.is_looping = false;
     }
+
+    fn framerate(&mut self, fps: usize) {
+        self.window.set_target_fps(fps);
+    }
+
+    fn rbg_color(r: u8, g: u8, b: u8) -> u32 {
+        ((r as u32) << 16) | ((g as u32) << 8) | b as u32
+    }
+
+    fn rgba_color(a: u8, r: u8, g: u8, b: u8) -> u32 {
+        ((a as u32) << 24) | ((r as u32) << 16) | ((g as u32) << 8) | b as u32
+    }
+
+    fn set_pixel(&mut self, x: u32, y: u32, color: u32) {
+        self.pixels[x as usize + y as usize * self.width] = color;
+    }
+
+    fn fill(&mut self, color: u32) {
+        self.fill_color = color;
+    }
+
+    fn fill_pixel(&mut self, x: u32, y: u32) {
+         self.set_pixel(x, y, self.fill_color);
+    }
+
+    fn rect(&mut self, x: u32, y: u32, w: u32, h: u32) {
+        for i in x..(x+w) {
+            for j in y..(y+h) {
+                self.fill_pixel(i, j);
+            }
+        }
+    }
+
+    fn background(&mut self, color: u32) {
+        for i in 0..self.width as u32 {
+            for j in 0..self.height as u32 {
+                self.set_pixel(i, j, color);
+            }
+        }
+    }
 }
 
 
@@ -71,14 +113,21 @@ mod tests {
     impl Runnable for Sketch {
         fn setup(&mut self) {
             println!("SETUP WAS CALLED");
+            self.framerate(60);
         }
 
         fn draw(&mut self) {
-            println!("DRAW WAS CALLED");
-
-            if self.frame_count >= 5 {
-                self.no_loop();
+            if self.frame_count == 0 {
+                println!("FIRST DRAW CALL");
             }
+
+            let blue: u32 = Self::rbg_color(50, 50, 255);
+            let green: u32 = Self::rbg_color(50, 255, 50);
+
+            self.background(blue);
+
+            self.fill(green);
+            self.rect(50, 100, 200, 100);
         }
     }
 
