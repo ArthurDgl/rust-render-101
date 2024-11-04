12b11abb28bdef23135d9e6ae9674a1500a7d588
Arthur <arthur.dugnolle@gmail.com>
2024-11-04 09:40:51 +0100

    color channels, handle mouse & keys, save screenshot

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,28 @@
 use minifb;
-use minifb::MouseMode;
+use minifb::{Key, KeyRepeat, MouseButton, MouseMode};
+use image::{ImageBuffer, Rgb};
 
 const DEFAULT_NAME: &str = "Rust Render 101 Sketch";
 
 pub trait Runnable {
     fn setup(&mut self);
     fn draw(&mut self);
+
+    fn mouse_pressed(&mut self) {
+        // Optional Implementation
+    }
+
+    fn mouse_released(&mut self) {
+        // Optional Implementation
+    }
+
+    fn key_pressed(&mut self, key: Key) {
+        // Optional Implementation
+    }
+
+    fn key_released(&mut self, key: Key) {
+        // Optional Implementation
+    }
 }
 
 pub struct Sketch {
@@ -21,6 +38,8 @@ pub struct Sketch {
 
     mouse_x: f32,
     mouse_y: f32,
+    mouse_is_pressed: bool,
+    mouse_button: MouseButton,
 
     fill_color: u32,
 }
@@ -34,7 +53,66 @@ impl Sketch {
 
         let pixels: Vec<u32> = vec![0; width*height];
 
-        Sketch {window, pixels, width, height, is_looping: true, frame_count: 0, delta_time: 0.0, mouse_x: 0.0, mouse_y: 0.0, fill_color: 0}
+        Sketch {
+            window,
+            pixels,
+            width,
+            height,
+            is_looping: true,
+            frame_count: 0,
+            delta_time: 0.0,
+            mouse_x: 0.0,
+            mouse_y: 0.0,
+            mouse_is_pressed: false,
+            mouse_button: MouseButton::Left,
+            fill_color: 0,
+        }
+    }
+
+    fn handle_mouse(&mut self) {
+        (self.mouse_x, self.mouse_y) = self.window.get_mouse_pos(MouseMode::Clamp).unwrap();
+
+        let temp = self.mouse_is_pressed;
+        if self.window.get_mouse_down(MouseButton::Left) {
+            self.mouse_is_pressed = true;
+            self.mouse_button = MouseButton::Left;
+        }
+        else if self.window.get_mouse_down(MouseButton::Right) {
+            self.mouse_is_pressed = true;
+            self.mouse_button = MouseButton::Right;
+        }
+        else if self.window.get_mouse_down(MouseButton::Middle) {
+            self.mouse_is_pressed = true;
+            self.mouse_button = MouseButton::Middle;
+        }
+        else {
+            if temp {
+                self.mouse_released();
+            }
+            self.mouse_is_pressed = false;
+        }
+
+        if !temp && self.mouse_is_pressed {
+            self.mouse_pressed();
+        }
+    }
+
+    fn key_is_down(&self, key: Key) -> bool{
+        self.window.is_key_down(key)
+    }
+
+    fn handle_keys(&mut self) {
+        let keys_pressed:Vec<Key> = self.window.get_keys_pressed(KeyRepeat::No);
+
+        for key in keys_pressed {
+            self.key_pressed(key);
+        }
+
+        let keys_released:Vec<Key> = self.window.get_keys_released();
+
+        for key in keys_released {
+            self.key_released(key);
+        }
     }
 
     fn run(&mut self) {
@@ -47,7 +125,8 @@ impl Sketch {
                 self.delta_time = now.elapsed().unwrap().as_secs_f32();
                 now = std::time::SystemTime::now();
 
-                (self.mouse_x, self.mouse_y) = self.window.get_mouse_pos(MouseMode::Clamp).unwrap();
+                self.handle_mouse();
+                self.handle_keys();
 
                 self.draw();
             }
@@ -74,6 +153,22 @@ impl Sketch {
 
     fn rgba_color(a: u8, r: u8, g: u8, b: u8) -> u32 {
         ((a as u32) << 24) | ((r as u32) << 16) | ((g as u32) << 8) | b as u32
+    }
+
+    fn color_alpha(color: u32) -> u8 {
+        (color >> 24) as u8
+    }
+
+    fn color_red(color: u32) -> u8 {
+        (color >> 16) as u8
+    }
+
+    fn color_green(color: u32) -> u8 {
+        (color >> 8) as u8
+    }
+
+    fn color_blue(color: u32) -> u8 {
+        color as u8
     }
 
     fn set_pixel(&mut self, x: u32, y: u32, color: u32) {
@@ -103,6 +198,21 @@ impl Sketch {
             }
         }
     }
+
+    fn save(&self, file_path: &str) {
+        let mut image = ImageBuffer::new(self.width as u32, self.height as u32);
+
+        for x in 0..self.width as u32 {
+            for y in 0..self.height as u32 {
+                let pixel: u32 = self.pixels[x as usize + y as usize * self.width];
+                image.put_pixel(x, y, Rgb([Self::color_red(pixel), Self::color_green(pixel), Self::color_blue(pixel)]));
+            }
+        }
+
+        image.save(file_path).unwrap_or_else(|e| {
+            panic!("Unable to save screenshot : {}", e);
+        });
+    }
 }
 
 
@@ -114,6 +224,9 @@ mod tests {
         fn setup(&mut self) {
             println!("SETUP WAS CALLED");
             self.framerate(60);
+
+            let blue: u32 = Self::rbg_color(50, 50, 255);
+            self.background(blue);
         }
 
         fn draw(&mut self) {
@@ -121,13 +234,25 @@ mod tests {
                 println!("FIRST DRAW CALL");
             }
 
-            let blue: u32 = Self::rbg_color(50, 50, 255);
             let green: u32 = Self::rbg_color(50, 255, 50);
-
-            self.background(blue);
 
             self.fill(green);
             self.rect(50, 100, 200, 100);
+        }
+
+        fn mouse_pressed(&mut self) {
+            let red: u32 = Self::rbg_color(255, 50, 50);
+
+            self.rect(self.mouse_x as u32, self.mouse_y as u32, 10, 10);
+        }
+
+        fn key_pressed(&mut self, key: Key) {
+            if key == Key::Space {
+                self.background(Self::rbg_color(0, 0, 0));
+            }
+            else if key == Key::S {
+                self.save("screenshot.png");
+            }
         }
     }
 
